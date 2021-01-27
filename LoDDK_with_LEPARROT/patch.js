e6a5acc67f7c33bdb9b992d7c4fe42b5072478f7
PartyParrotTH <oowo52513@gmail.com>
2021-01-27 11:50:23 +0700

    Updated Files

--- LoDDK_with_LEPARROT/patch.js
+++ LoDDK_with_LEPARROT/patch.js
@@ -4,7 +4,7 @@
 	],
 	"id": "LoDDK_with_LEPARROT",
 	"servers": [
-	"https://raw.githubusercontent.com/PartyParrotTH/PartyParrot-s-Patches-Repository/master/LoDDK_with_LEPARROT/"
+		"https://raw.githubusercontent.com/PartyParrotTH/PartyParrot-s-Patches-Repository/master/LoDDK_with_LEPARROT/"
 	],
 	"title": "(TH15) Boss Rush With DDC And LoLK Combined LEPARROT!"
 }
